73c83e3364ae8e8fd86077d432f8231704b6c7c5
Joe Flintham <joeflintham@gmail.com>
2015-09-18 12:29:48 +0100

    Remove BRP-specific tests, routes, controllers, fields, translations, and templates

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,6 @@
 
 var app = require('express').Router();
 var rtmSteps = require('./steps/rtm');
-var deliverySteps = require('./steps/delivery');
-var errorSteps = require('./steps/error');
-var lostSteps = require('./steps/lost');
-var collectionSteps = require('./steps/collection');
 var fields = require('./fields');
 var wizard = require('hmpo-form-wizard');
 var mixins = require('hmpo-template-mixins');
@@ -18,14 +14,6 @@ var options = {
 app.use(mixins(i18n.translate.bind(i18n), fields));
 
 app.use('/terrorism-reporting', wizard(rtmSteps, fields, options));
-
-app.use('/not-arrived/', wizard(deliverySteps, fields, options));
-
-app.use('/correct-mistakes/', wizard(errorSteps, fields, options));
-
-app.use('/lost-stolen-damaged/', wizard(lostSteps, fields, options));
-
-app.use('/collection/', wizard(collectionSteps, fields, options));
 
 app.use('/cookies', function cookies(req, res) {
   res.render('cookies');
