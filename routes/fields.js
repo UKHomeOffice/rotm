d64829c832533bcae6d37e9e671fab54cc1f359b
Joe Flintham <joeflintham@gmail.com>
2015-10-09 16:01:25 +0100

    Main form edited according to first UX draft ROTM-12

--- routes/fields.js
+++ routes/fields.js
@@ -1,22 +1,9 @@
 'use strict';
 
 module.exports = {
-  'continue': {
-    value: 'buttons.continue'
-  },
-  submit: {
-    value: 'buttons.submit'
-  },
-  change: {
-    value: 'buttons.change'
-  },
   'website_url': {
     validate: ['required'],
     label: 'fields.website_url.label'
-  },
-  'trigger_warning': {
-    label: 'fields.trigger_warning.label',
-    className: 'form-checkbox'
   },
   'content_locate_hint': {
     label: 'fields.content_locate_hint.label'
