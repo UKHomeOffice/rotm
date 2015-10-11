857e814e3d6844d86612f473f1a43a4a3b0e172c
Daniel A.C. Martin <github@daniel-martin.co.uk>
2015-10-11 19:54:12 +0100

    Clean-up code

--- controllers/add-report.js
+++ controllers/add-report.js
@@ -1,12 +1,11 @@
 'use strict';
 
 var util = require('util');
-var _ = require('underscore');
 
 var Controller = require('../lib/add-to-session-controller');
 
 var AddReport = function AddReport() {
-  [].unshift.call(arguments, 'report')
+  [].unshift.call(arguments, 'report');
   Controller.apply(this, arguments);
 };
 
