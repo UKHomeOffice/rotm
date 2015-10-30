28167261c7fefa5ccaeccb6381c6c59d03058589
Joe Flintham <joeflintham@gmaail.com>
2015-10-30 19:21:17 +0000

    Edit run script

--- run.sh
+++ run.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# copy assets over to /public for nginx container to serve, only copy if /public
-# directory exists, which implies that kubernetes volume has been mounted
-PUBLIC_DIR='/public'
-if [[ -d ${PUBLIC_DIR} ]] && [[ ! $(ls -A ${PUBLIC_DIR}) ]]; then
-  cp -r /app/public/ /
+if [ ${DOCKER_COMPOSE} ]
+  then npm run dev;
+  else npm start;
 fi
 
-npm start
