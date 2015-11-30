3838fa48779067f33244a5b6fd92d5d8ad3f6115
Joe Flintham <joeflintham@gmail.com>
2015-11-30 16:07:09 +0000

    Stop script checks for pid

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+rtm_node_process_id=$(pidof rtm)
+if [ -z rtm_node_process_id ]
+then npm stop
+fi
+  
 if [ "$NODE_ENV" = "development" ]
   then npm run dev;
 elif [ "$NODE_ENV" = "so-ci" ]
