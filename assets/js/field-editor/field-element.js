65ba57cf19cbd52c7ec0695f7798478dc8152a2f
Joe Flintham <joeflintham@gmail.com>
2015-12-01 13:50:39 +0000

    Remove inline editing

--- assets/js/field-editor/field-element.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-/*eslint no-undef: 0 */
-/*eslint no-unused-vars: 0 */
-/*eslint func-names: 0 */
-var FieldElement = function(tagName) {
-  return document.createElement(tagName);
-};
-
-module.exports = FieldElement;
