e9e74d124d3ec43e03fe4026c63c31c9007aea6c
Joe Flintham <joeflintham@gmail.com>
2015-12-02 10:29:22 +0000

    Testing start-up script

--- run.sh
+++ run.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-rtm_node_process_id=$(pidof rtm)
-if [ -z rtm_node_process_id ]
-then npm stop
+if pidof -o %PPID -x "rtm">/dev/null; then
+   echo "stopping pid $$"
+   npm stop
 fi
-  
+
 if [ "$NODE_ENV" = "development" ]
-  then npm run dev;
+then echo "starting the service"
+     npm run dev
+elif [ "$NODE_ENV" = "nginx-dev" ]
+then echo "starting the service"
+     SITEROOT=/rotm nohup nodemon -e html,js,json --debug . > /dev/null 2>&1 &
+     exit 0
 elif [ "$NODE_ENV" = "so-ci" ]
-  then SITEROOT=/rotm nohup npm start > /dev/null 2>&1 &
-  exit 0
+then echo "starting service"
+     SITEROOT=/rotm nohup npm start > /dev/null 2>&1 &
+     exit 0
 fi
 
 
