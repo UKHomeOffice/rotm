26f885df445f7e2f764770cfa9065341f33c3458
Joe Flintham <joeflintham@gmail.com>
2015-12-16 09:12:47 +0000

    Implement a reset path for the purpose of clearing the session

---
+++ apps/rtm/controllers/reset.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var util = require('util');
+var _ = require('underscore');
+var logger = require('../../../lib/logger');
+
+var Controller = require('../../../lib/ajax-edit-controller');
+
+var Reset = function Reset() {
+  Controller.apply(this, arguments);
+};
+
+util.inherits(Reset, Controller);
+
+/*eslint no-unused-vars: 0*/
+Reset.prototype.saveValues = function getValues(req, res, callback) {
+  req.sessionModel.reset();
+  callback();
+};
+
+Reset.prototype.getNextStep = function getNextStep(req, res) {
+  var next = res.locals.siteroot + req.baseUrl;
+  return next;
+};
+
+module.exports = Reset;
