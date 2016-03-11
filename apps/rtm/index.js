5baadc8d67da51ffef328de28f49d9e17be065ab
Joe Flintham <joeflintham@gmail.com>
2016-03-11 14:31:39 +0000

    Replace hof-transpiled translations with jason-baked translations

--- apps/rtm/index.js
+++ apps/rtm/index.js
@@ -3,25 +3,39 @@
 var hof = require('hof');
 var wizard = hof.wizard;
 var mixins = hof.mixins;
-var i18nFuture = hof.i18n;
+var i18nFuture = require('i18n-future');
 var router = require('express').Router();
 var path = require('path');
 var _ = require('underscore');
+var jasonBaker = require('jason-baker');
 
 var fields = _.extend({}, require('../common/fields/'), require('./fields/'));
-var i18n = i18nFuture({
-  path: path.resolve(__dirname, './translations/__lng__/__ns__.json')
+var translationSrc = [
+  path.resolve(__dirname, './translations/src/**/*.json'),
+  path.resolve(__dirname, '../common/translations/src/**/*.json')
+];
+
+jasonBaker(translationSrc, {saltStars: true}, function setupRoute(err, translations) {
+
+  if (err) {
+    throw err;
+  }
+
+  var i18n = i18nFuture({
+    resources: translations
+  });
+
+  router.use(mixins(fields, {
+    translate: i18n.translate.bind(i18n)
+  }));
+
+  router.use('/report-terrorism/', wizard(require('./steps'), fields, {
+    controller: require('../../lib/base-controller'),
+    templatePath: path.resolve(__dirname, 'views'),
+    translate: i18n.translate.bind(i18n),
+    params: '/:action?'
+  }));
+
 });
-
-router.use(mixins(fields, {
-  translate: i18n.translate.bind(i18n)
-}));
-
-router.use('/report-terrorism/', wizard(require('./steps'), fields, {
-  controller: require('../../lib/base-controller'),
-  templatePath: path.resolve(__dirname, 'views'),
-  translate: i18n.translate.bind(i18n),
-  params: '/:action?'
-}));
 
 module.exports = router;
