01b65aca190ad63600c66819add179182ad5f17c
Joe Flintham <joeflintham@gmail.com>
2016-03-17 13:03:53 +0000

    Convert controllers to es6

--- apps/rtm/controllers/confirm.js
+++ apps/rtm/controllers/confirm.js
@@ -1,103 +1,104 @@
 'use strict';
 
-var util = require('util');
-var _ = require('underscore');
-var path = require('path');
-var uuid = require('node-uuid');
-var i18nFuture = require('i18n-future')();
+const _ = require('lodash');
+const path = require('path');
+const uuid = require('node-uuid');
+const i18n = require('i18n-future');
 
-var Controller = require('./rotm-base-controller');
-var Model = require('../../common/models/email');
+const Controller = require('./rotm-base-controller');
+const Model = require('../../common/models/email');
 
-var Submit = function Submit() {
-  Controller.apply(this, arguments);
-};
-
-util.inherits(Submit, Controller);
-
-Submit.prototype.getValues = function getValues(req) {
-  var data = this.getReports(req);
-
-  if (!this.options.hasOwnProperty('originalTemplate')) {
-    this.options.originalTemplate = this.options.template;
+module.exports = class Submit extends Controller {
+  constructor(options) {
+    super(options);
   }
 
-  if (!data.length) {
-    this.options.template = path.resolve(__dirname, '../../common/views/start-again.html');
-  } else {
-    this.options.template = this.options.originalTemplate;
-  }
+  getValues(req, res, callback) {
+    const data = super.getReports(req);
 
-  _.each(data, function addIndex(d, i) {
+    if (!this.options.hasOwnProperty('originalTemplate')) {
+      this.options.originalTemplate = this.options.template;
+    }
 
-    var options = {
-      'id': i,
-      'uri': 'editurl'
-    };
-    var defaults = {};
+    if (!data.length) {
+      this.options.template = path.resolve(__dirname, '../../common/views/start-again.html');
+    } else {
+      this.options.template = this.options.originalTemplate;
+    }
 
-    d.options = options;
+    _.each(data, function addIndex(d, i) {
 
-    d['data-options'] = JSON.stringify(options);
-    d['data-defaults'] = JSON.stringify(defaults);
-    d['report-number'] = i + 1;
-  });
-  Controller.prototype.getValues.apply(this, arguments);
-};
+      const options = {
+        'id': i,
+        'uri': 'editurl'
+      };
+      const defaults = {};
 
-Submit.prototype.locals = function locals(req) {
-  var lcls = Controller.prototype.locals.apply(this, arguments);
-  var reportCount = this.getReports(req).length;
-  lcls['single-report'] = reportCount === 1;
-  lcls['multiple-reports'] = reportCount > 1;
-  return lcls;
-};
+      d.options = options;
 
-Submit.prototype.saveValues = function saveValues(req, res, callback) {
-  var data = this.getReports(req);
-
-  if (data && data.length) {
-
-    var userData = req.form.values.anonymous === 'no' ? {
-      'contact-details': true,
-      name: req.form.values['contact-info-name'],
-      email: req.form.values['contact-info-email'],
-      phone: req.form.values['contact-info-phone']
-    } : {};
-
-    i18nFuture.on('ready', function prepareEmail() {
-
-      data.forEach(function sendEachReport(d, i) {
-
-        var reportNumber = i + 1;
-        var subjectAppend = data.length ? ' - ' + reportNumber + ' of ' + data.length : '';
-
-        d = _.extend(d, userData);
-
-        var dateTime = new Date();
-        d.reportDate = dateTime.toISOString();
-        d.reportId = uuid.v4();
-        d.subject = i18nFuture.translate('pages.rtm-email-table.information.subject') + subjectAppend;
-
-        var model = new Model(d);
-        var service = {
-          template: 'rtm',
-          subject: d.subject
-        };
-
-        if (service) {
-          model.set('template', service.template);
-          model.set('subject', service.subject);
-        } else {
-          throw new Error('no service found');
-        }
-
-        model.save(callback);
-
-      });
+      d['data-options'] = JSON.stringify(options);
+      d['data-defaults'] = JSON.stringify(defaults);
+      d['report-number'] = i + 1;
     });
+    super.getValues(req, res, callback);
+  }
 
+  locals(req) {
+    const lcls = super.locals(req);
+    const reportCount = super.getReports(req).length;
+    lcls['single-report'] = reportCount === 1;
+    lcls['multiple-reports'] = reportCount > 1;
+    return lcls;
+  }
+
+  saveValues(req, res, callback) {
+    const data = super.getReports(req);
+
+    if (data && data.length) {
+
+      const userData = req.form.values.anonymous === 'no' ? {
+        'contact-details': true,
+        name: req.form.values['contact-info-name'],
+        email: req.form.values['contact-info-email'],
+        phone: req.form.values['contact-info-phone']
+      } : {};
+
+      const locali18n = i18n({
+        path: path.resolve(
+        )
+      });
+
+      locali18n.on('ready', function prepareEmail() {
+
+        data.forEach(function sendEachReport(d, i) {
+
+          const reportNumber = i + 1;
+          const subjectAppend = data.length ? ' - ' + reportNumber + ' of ' + data.length : '';
+
+          d = _.extend(d, userData);
+
+          const dateTime = new Date();
+          d.reportDate = dateTime.toISOString();
+          d.reportId = uuid.v4();
+          d.subject = locali18n.translate('pages.rtm-email-table.information.subject') + subjectAppend;
+
+          const model = new Model(d);
+          const service = {
+            template: 'rtm',
+            subject: d.subject
+          };
+
+          if (service) {
+            model.set('template', service.template);
+            model.set('subject', service.subject);
+          } else {
+            throw new Error('no service found');
+          }
+
+          model.save(callback);
+        });
+      });
+
+    }
   }
 };
-
-module.exports = Submit;
