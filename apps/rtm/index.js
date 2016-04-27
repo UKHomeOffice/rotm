3985a7e03aec99668d2f158375839e43799a67aa
Ben Diggle <ben.diggle@transformuk.com>
2016-04-27 10:58:52 +0100

    Moved the json translations out of the default directory back ito the en directory. Done for both common and rtm directories Removed jason-baker from package.json. Added hof-transpiler dependancy and added hof-transpile npm script Removed jason-baker from RTM index file

--- apps/rtm/index.js
+++ apps/rtm/index.js
@@ -7,37 +7,21 @@ var i18nFuture = require('i18n-future');
 var router = require('express').Router();
 var path = require('path');
 var _ = require('underscore');
-var jasonBaker = require('jason-baker');
 
 var fields = _.extend({}, require('../common/fields/'), require('./fields/'));
-var translationSrc = [
-  path.resolve(__dirname, './translations/src/**/*.json'),
-  path.resolve(__dirname, '../common/translations/src/**/*.json')
-];
+var i18n = i18nFuture({
+  path: path.resolve(__dirname, './translations/__lng__/__ns__.json')
+});
 
-function setupRoute(translations) {
+router.use(mixins(fields, {
+  translate: i18n.translate.bind(i18n)
+}));
 
-  var i18n = i18nFuture({
-    resources: translations
-  });
-
-  router.use(mixins(fields, {
-    translate: i18n.translate.bind(i18n)
-  }));
-
-  router.use('/report-terrorism/', wizard(require('./steps'), fields, {
-    controller: require('so-forms').controllers.base,
-    templatePath: path.resolve(__dirname, 'views'),
-    translate: i18n.translate.bind(i18n),
-    params: '/:action?'
-  }));
-
-}
-
-jasonBaker.promise(translationSrc, {fsToNs: true})
-  .then(setupRoute)
-  .catch(function catchError(err) {
-    throw err;
-  });
+router.use('/report-terrorism/', wizard(require('./steps'), fields, {
+  controller: require('so-forms').controllers.base,
+  templatePath: path.resolve(__dirname, 'views'),
+  translate: i18n.translate.bind(i18n),
+  params: '/:action?'
+}));
 
 module.exports = router;
