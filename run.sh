9451ec56ac7a012d25deee5409c8346e5c919b2c
Ben Diggle <ben.diggle@transformuk.com>
2016-04-29 15:58:29 +0100

    Removed env variable SERVE_STATIC from config.js and all its references including the ci_build and run sh scripts and the travis config file

--- run.sh
+++ run.sh
@@ -7,12 +7,9 @@ REDIS_HOST=${REDIS_HOST:-127.0.0.1}
 REDIS_PORT=${REDIS_PORT:-6379}
 SITEROOT=${SITEROOT:-/rotm}
 GA_TAG_ID=${GA_TAG_ID}
-SERVE_STATIC=${SERVE_STATIC}
 echo "starting the service"
 
-if [ "${SERVE_STATIC}" == "" ]; then
-  cp -r /app/public/* /public/
-fi
+cp -r /app/public/* /public/
 SECURE_PROTOCOL=true npm start
 
 
