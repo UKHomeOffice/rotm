93ff2295b2d913b5f0808ee66ff12fba234d8738
Vijay Pujar <vijay_pujar@yahoo.com>
2016-09-06 12:24:37 +0100

    amended the tests to load page tests

--- acceptance_tests/features/step_definitions/LoadPage.steps.rb
+++ acceptance_tests/features/step_definitions/LoadPage.steps.rb
@@ -9,18 +9,6 @@ Then(/^I should see the RTM form$/) do
   expect(find('form').find('#url-group')).to have_selector('input#url')
 end
 
-Then(/^I visit the cookie page$/) do
-  visit config['rtm_dev_host'] + '/cookies'
-  page.status_code.should == 200
-  page.should have_content('Cookies');
-end
-
-Then(/^I visit the terms page$/) do
-  visit config['rtm_dev_host'] + '/terms-and-conditions'
-  page.status_code.should == 200
-  page.should have_content('Terms and conditions');
-end
-
 When(/^I load the bundle JS asset$/) do
   visit config['rtm_dev_host'] + '/public/js/bundle.js'
 end
@@ -28,3 +16,29 @@ end
 Then(/^I should get a (\d+) status$/) do |status_code|
   page.status_code.should == status_code.to_i
 end
+
+Then(/^I visit the cookie page$/) do
+  click_link('Cookies')
+  page.status_code.should == 200
+  page.should have_content('Cookies');
+end
+
+Then(/^I visit the terms page$/) do
+  click_link('Terms and conditions')
+  page.status_code.should == 200
+  page.should have_content('Terms and conditions');
+end
+
+Then(/^I visit the ogl page$/) do
+  page.find(:xpath, "//*[@id='footer']/div/div/div[1]/div/p[2]/a").click  
+  page.status_code.should == 200
+  page.should have_title('Open Government Licence'); 
+end
+
+Then(/^I visit the CrownCopyright page$/) do
+  page.find(:xpath, "//*[@id='footer']/div/div/div[2]/a").click  
+  page.status_code.should == 200
+  page.should have_title('Crown copyright - The National Archives');
+end
+
+
