9e73b074b9b60bbc2d51aed6d5b2371b21a84227
Joe Fitter <joefitter@hotmail.com>
2016-10-21 12:32:27 +0100

    feature RTMD-89 - Contact Consent step (#88)
    * added contact consent step
    * removed a few superfluous templates

--- apps/rotm/acceptance/pages/contact-consent.js
+++ apps/rotm/acceptance/pages/contact-consent.js
@@ -1,5 +1,10 @@
 'use strict';
 
 module.exports = {
-  url: 'contact-consent'
+  url: 'contact-consent',
+  id: {
+    consent: '#contact-consent-group',
+    no: '[value=false]',
+    yes: '[value=true]'
+  }
 };
