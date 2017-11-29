0458415fc91b42cddd836e5fa1a462ac7d9e32c5
Leonard Martin <leonard.martin@gmail.com>
2017-11-29 17:21:24 +0000

    Add image upload test

---
+++ apps/rotm/acceptance/features/image-upload.js
@@ -0,0 +1,12 @@
+'use strict';
+
+Feature('Image Upload');
+
+Scenario('I can upload an image', (
+  I
+) => {
+  I.amOnPage('/');
+  I.completeToStep('/image');
+  I.attachFile('input[name="file-upload"]', './apps/rotm/acceptance/screenshot.png');
+  I.seeInCurrentUrl('/add-image');
+});
