ac73bd718fa0ebd87e6e0b8d562e8f2243214ee5
Leonard Martin <leonard.martin@gmail.com>
2017-12-22 11:20:49 +0000

    Add support for configurable test urls (#134)
    * Add support for configurable test urls
    
    This allows us to easily run acceptance tests against multiple environments. Including dev, preprod and prod.
    
    * Add smoke test runner to drone
    
    This will allow the CI to run our test suite against the deployed environment following a deployment.

--- codecept.conf.js
+++ codecept.conf.js
@@ -1,4 +1,5 @@
 'use strict';
+/* eslint no-process-env: 0 */
 /* eslint implicit-dependencies/no-implicit: [2, { dev: true }] */
 module.exports = require('so-acceptance').extend({
   name: 'rotm',
@@ -8,7 +9,7 @@ module.exports = require('so-acceptance').extend({
       host: 'localhost',
       port: 4444,
       path: '/wd/hub',
-      url: 'http://localhost:8080',
+      url: process.env.TEST_URL || 'http://localhost:8080',
       browser: 'chrome',
       desiredCapabilities: {
         chromeOptions: { args: ['headless', 'disable-gpu'] }
