c80f4fa696cb1d5fcb19b5f7b1dbf7d2b2df7606
Sulthan Ahmed <sulthanahmeduk@gmail.com>
2018-03-27 11:07:02 +0100

    [RTMD-179] - Accessibility: remove tab focus for hidden elements
    * Some hidden elements should not have tab focus.  This is for screenreader and normal users.   It is confusing.
    * There are different items hidden when JS is enabled and disabled.  This deals with both scenarios

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,6 +4,12 @@ require('hof-theme-govuk');
 
 document.getElementById('add-image-button').classList.remove('visuallyhidden');
 
+document.getElementById('file-upload').tabIndex = -1;
+
+document.getElementById('image-submit').tabIndex = -1;
+
+document.getElementById('add-image-button').removeAttribute('tabindex');
+
 document.getElementById('add-image-button').addEventListener('click', function imgClick() {
   document.getElementById('file-upload').click();
 });
