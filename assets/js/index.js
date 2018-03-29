1f00cc3ab343fa638b876e5390c1340b2d06993f
Sulthan Ahmed <sulthanahmeduk@gmail.com>
2018-03-29 11:36:53 +0100

    [RTMD-178] - Accessibility - focus on validation-summary when it appears
    Note: this does not work if JS is disabled.  However, the guidance is that an app should be functional when JS is disabled.  It is still functional

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,9 @@
 
 require('hof-theme-govuk');
 
+// accessibility- focus on the validation when it appears
+document.getElementsByClassName('validation-summary')[0].focus();
+
 document.getElementById('add-image-button').classList.remove('visuallyhidden');
 
 document.getElementById('add-image-button').addEventListener('click', function imgClick() {
