0c76088ee8e5e23dffe714b8b79afd7e4815fe2c
Ben Marvell <madmarv187@gmail.com>
2018-09-01 18:20:34 +0100

    Use funkie

--- codecept.conf.js
+++ codecept.conf.js
@@ -5,7 +5,7 @@ const path = require('path');
 const pagesPath = filename => path.resolve(__dirname,
   `./apps/rotm/acceptance/pages/${filename}`);
 
-module.exports = {
+module.exports = require('so-acceptance').extend({
   name: 'rotm',
   features: './apps/*/acceptance/features/**/*.js',
   include: {
@@ -14,5 +14,17 @@ module.exports = {
     contactDetailsPage: pagesPath('contact-details.js'),
     confirmPage: pagesPath('confirm.js'),
     confirmationPage: pagesPath('confirmation.js')
+  },
+  helpers: {
+    WebDriverIO: {
+      host: 'localhost',
+      port: 4444,
+      path: '/wd/hub',
+      url: process.env.TEST_URL || 'http://localhost:8080',
+      browser: 'chrome',
+      desiredCapabilities: {
+        chromeOptions: { args: ['headless', 'disable-gpu'] }
+      }
+    }
   }
-};
+});
