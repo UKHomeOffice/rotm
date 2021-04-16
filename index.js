85329d213ba14294f477a135bb0679f7ee562ea3
christopher medland <christopher.medland@digital.homeoffice.gov.uk>
2021-04-16 09:58:47 +0100

    SD-237 update to include compliant cookie banner

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const hof = require('hof');
 const config = require('./config');
 const mockAPIs = require('./mock-apis');
 const bodyParser = require('busboy-body-parser');
+const fs = require('fs');
 
 if (process.env.REDIS_URL) {
   config.redis = process.env.REDIS_URL;
@@ -30,12 +31,25 @@ if (config.useMocks) {
 }
 
 app.use((req, res, next) => {
+  res.locals.htmlLang = 'en';
   res.locals.footerSupportLinks = [
     { path: '/cookies', property: 'base.cookies' },
     { path: '/terms-and-conditions', property: 'base.terms' },
     { path: '/accessibility', property: 'base.accessibility' },
   ];
   next();
+});
+
+fs.readFile('node_modules/hof-template-partials/translations/src/en/cookies.json', 'utf8', (e, data) => {
+  const obj = JSON.parse(data);
+  obj['session-cookies-table'].rows[0][0] = 'hod.sid';
+  fs.writeFile('node_modules/hof-template-partials/translations/src/en/cookies.json',
+    JSON.stringify(obj, null, 2), (err) => {
+      if (err) {
+        // eslint-disable-next-line no-console
+        console.err(err);
+      }
+    });
 });
 
 app.use(bodyParser({limit: config.upload.maxFileSize}));
