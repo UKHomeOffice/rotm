4c8bd4923373a90755476954561c2abf212f5d99
Alexander Swann <alexander.swann@digital.homeoffice.gov.uk>
2021-08-22 14:38:11 +0100

    [PIPELINE]: Add missing service file to deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -26,7 +26,7 @@ if [[ ${KUBE_NAMESPACE} == ${BRANCH_ENV} ]]; then
   $kd -f kube/configmaps -f kube/certs
   $kd -f kube/redis -f kube/file-vault -f kube/app
 elif [[ ${KUBE_NAMESPACE} == ${UAT_ENV} ]]; then
-  $kd -f kube/configmaps/configmap.yml
+  $kd -f kube/configmaps/configmap.yml  -f kube/app/service.yml
   $kd -f kube/app/ingress-external.yml -f kube/app/networkpolicy-external.yml
   $kd -f kube/redis -f kube/file-vault -f kube/app/deployment.yml
 elif [[ ${KUBE_NAMESPACE} == ${STG_ENV} ]]; then
