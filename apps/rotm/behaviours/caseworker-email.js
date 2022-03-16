11d8a0287826d5f8bbedcfb136809ff4093b7328
Sarah-Jane Luff <sarah-jane.luff@digital.homeoffice.gov.uk>
2022-03-16 11:27:55 +0000

    test logs

--- apps/rotm/behaviours/caseworker-email.js
+++ apps/rotm/behaviours/caseworker-email.js
@@ -4,22 +4,24 @@ const Emailer = require('hof').components.emailer;
 const path = require('path');
 const moment = require('moment');
 const config = require('../../../config');
+const submissionDateTime = moment().format(config.dateTimeFormat);
 
 const parse = (model, translate) => {
   const getLabel = key => translate(`email.caseworker.fields.${key}.label`);
-  const submissionDateTime = moment().format(config.dateTimeFormat);
   const fields = [
     'evidence-written',
     'contact-details-name',
     'contact-email',
     'contact-phone'
   ];
-  model.log('info', `Submission ID: ${model.submissionID}, Submitted: ${submissionDateTime}`);
+
+  //model.log('info', `Submission ID: ${model.submissionID}, Submitted: ${submissionDateTime}`);
+
   return {
     urls: model.urls,
     images: model.images,
     table: [
-      { label: getLabel('uniqueId'), value: '000000000000' }, // model.submissionID
+      { label: getLabel('uniqueId'), value: model.submissionID },
       { label: getLabel('submitted'), value: submissionDateTime },
       ...fields.map(f => ({
         label: getLabel(f),
@@ -30,6 +32,11 @@ const parse = (model, translate) => {
 };
 
 module.exports = settings => {
+
+  /*
+  model.log('info', `Submission ID: ${req.sessionModel.get('submissionID')},
+                     Submitted: ${submissionDateTime}`); */
+
   if (settings.transport !== 'stub' && !settings.from && !settings.replyTo) {
     // eslint-disable-next-line no-console
     console.warn('WARNING: Email `from` address must be provided. Falling back to stub email transport.');
