61316b7a6a499df273adcb9e25913e5a6557a342
Rhodine Orleans-Lindsay <43888758+Rhodine-orleans-lindsay@users.noreply.github.com>
2024-03-08 19:17:12 +0000

    HOFF-688: Update ROTM to use version of HOF using axios
    - update hof version to use axios
    - update playwright version to fix breaking acceptance test
    - add mocha as dev dependency to run unit tests
    - update yarn.lock
    - update node version to version 20
    - refactor image-upload model to work with hof model
    - update snyk ignore list
    - remove anchore scan steps in drone.yml
    - add PR template
    - change 'deploy to branch' message in deploy.sh from 'UAT Branch' to 'Branch' to avoid confusion in drone step

--- apps/rotm/models/image-upload.js
+++ apps/rotm/models/image-upload.js
@@ -2,14 +2,14 @@
 'use strict';
 
 const url = require('url');
-const Model = require('hof').model;
+const { model: Model } = require('hof');
 const jimp = require('jimp');
 const uuid = require('uuid').v4;
 const fs = require('fs');
-const noPreview = 'data:image/png;base64,' + fs.readFileSync('assets/images/no-preview.png', {encoding: 'base64'});
-
-
+const noPreview = 'data:image/png;base64,' + fs.readFileSync('assets/images/no-preview.png', { encoding: 'base64' });
+const FormData = require('form-data');
 const config = require('../../../config');
+const logger = require('hof/lib/logger')({ env: config.env });
 
 module.exports = class UploadModel extends Model {
   constructor(...args) {
@@ -17,88 +17,84 @@ module.exports = class UploadModel extends Model {
     this.set('id', uuid());
   }
 
-  save() {
-    return new Promise((resolve, reject) => {
+  async save() {
+    try {
       const attributes = {
         url: config.upload.hostname
       };
       const reqConf = url.parse(this.url(attributes));
-      reqConf.formData = {
-        document: {
-          value: this.get('data'),
-          options: {
-            filename: this.get('name'),
-            contentType: this.get('mimetype')
-          }
-        }
-      };
+      const formData = new FormData();
+      formData.append('document', this.get('data'), {
+        filename: this.get('name'),
+        contentType: this.get('mimetype')
+      });
+      reqConf.data = formData;
       reqConf.method = 'POST';
-      this.request(reqConf, (err, data) => {
-        if (err) {
-          return reject(err);
-        }
-        resolve(data);
-      });
-    })
-      .then(result => {
-        return this.set({ url: result.url });
-      })
-      .then(() => {
-        return this.thumbnail();
-      })
-      .then(() => {
-        return this.unset('data');
-      });
-  }
-
-  thumbnail() {
-    return jimp.read(this.get('data'))
-      .then(image => {
-        image.resize(300, jimp.AUTO);
-        return new Promise((resolve, reject) => {
-          image.getBase64(this.get('mimetype'), (e, data) => {
-            return e ? reject(e) : resolve(data);
-          });
+      reqConf.headers = {
+        ...formData.getHeaders()
+      };
+      const result = await new Promise((resolve, reject) => {
+        this.request(reqConf, (err, data) => {
+          if (err) {
+            return reject(err);
+          }
+          return resolve(data);
         });
-      })
-      .then(data => {
-        this.set('thumbnail', data);
-      })
-      .catch(() => {
-        this.set('thumbnail', noPreview);
       });
-  }
-
-  auth() {
-    if (!config.keycloak.token) {
-      // eslint-disable-next-line no-console
-      console.error('keycloak token url is not defined');
-      return Promise.resolve({
-        bearer: 'abc123'
-      });
+      await this.set({ url: result.url });
+      await this.thumbnail();
+      this.unset('data');
+    } catch (error) {
+      logger.error('Error in save method ', error);
     }
-    const tokenReq = {
-      url: config.keycloak.token,
-      form: {
-        username: config.keycloak.username,
-        password: config.keycloak.password,
-        grant_type: 'password',
-        client_id: config.keycloak.clientId,
-        client_secret: config.keycloak.secret
-      },
-      method: 'POST'
-    };
+  }
 
-    return new Promise((resolve, reject) => {
-      this._request(tokenReq, (err, response) => {
-        if (err) {
-          return reject(err);
-        }
+  async thumbnail() {
+    try {
+      const image = await jimp.read(this.get('data'));
+      image.resize(300, jimp.AUTO);
 
-        resolve({
-          bearer: JSON.parse(response.body).access_token
+      const thumbnail = await new Promise((resolve, reject) => {
+        image.getBase64(this.get('mimetype'), (e, data) => {
+          if (e) {
+            reject(e);
+          } else {
+            resolve(data);
+          }
         });
       });
-    });
+
+      this.set('thumbnail', thumbnail);
+    } catch (error) {
+      this.set('thumbnail', noPreview);
+    }
+  }
+
+  async auth() {
+    try {
+      if (!config.keycloak.token) {
+        logger.error('keycloak token url is not defined');
+        return Promise.resolve({
+          bearer: 'abc123'
+        });
+      }
+      const tokenReq = {
+        url: config.keycloak.token,
+        headers: { 'content-type': 'application/x-www-form-urlencoded' },
+        data: {
+          username: config.keycloak.username,
+          password: config.keycloak.password,
+          grant_type: 'password',
+          client_id: config.keycloak.clientId,
+          client_secret: config.keycloak.secret
+        },
+        method: 'POST'
+      };
+      const response = await this._request(tokenReq);
+      return { bearer: response.data.access_token };
+    } catch (err) {
+      logger.error(`Error: ${err.response.data.error} - ${err.response.data.error_description}`);
+      throw err;
+    }
   }
 };
