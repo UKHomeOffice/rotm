0b225128c9f6a1b2c1b3db6f83371d2aa328ddc4
TemitopeAyokuHO <temitope.ayoku@digital.homeoffice.gov.uk>
2024-03-21 10:26:46 +0000

    test

--- test/_features/step_definitions/steps.js
+++ test/_features/step_definitions/steps.js
@@ -83,7 +83,7 @@ Then('I enter a {string} date of birth for a {int} year old', async function (fi
 
 Then('I should be on the {string} page showing {string}', async function (uri, heading) {
   await this.page.waitForSelector('body', { timeout: 15000 });
-  // expect(new URL(await this.page.url()).pathname).to.eql(`${this.subApp}/${uri}`);
+  expect(new URL(await this.page.url()).pathname).to.eql(`${this.subApp}/${uri}`);
   expect(await this.page.innerText('body')).to.include(heading);
 }.bind(World));
 
