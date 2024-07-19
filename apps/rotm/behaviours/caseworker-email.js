8c361cbe415d49a4c6752c9c84cc260219f6a4af
shamiluwais <mohamed.uwais@digital.homeoffice.gov.uk>
2024-07-19 16:22:43 +0100

    fix lint errors

--- apps/rotm/behaviours/caseworker-email.js
+++ apps/rotm/behaviours/caseworker-email.js
@@ -9,9 +9,9 @@ const { createLogger, format, transports } = require('winston');
 const { combine, timestamp, json } = format;
 const http = require('http');
 
-let ipAddress = "";
-http.get({'host': 'api.ipify.org', 'port': 80, 'path': '/'}, function(resp) {
-  resp.on('data', function(ip) {
+let ipAddress = '';
+http.get({host: 'api.ipify.org', port: 80, path: '/'}, function (resp) {
+  resp.on('data', function (ip) {
     ipAddress = String.fromCharCode(...ip);
   });
 });
@@ -56,7 +56,7 @@ const parse = (model, translate) => {
 };
 
 module.exports = settings => {
-return Notify(Object.assign({}, settings, {
+  return Notify(Object.assign({}, settings, {
     recipient: settings.caseworker,
     subject: (model, translate) => translate('email.caseworker.subject'),
     template: path.resolve(__dirname, '../emails/caseworker.html'),
