15784537ad4e8a0c6ed197f2d0e0cb17727780d0
dk4g <157156245+dk4g@users.noreply.github.com>
2025-12-16 10:43:23 +0000

    ROTM-179: Google reCAPTCHA Implementation (#305)

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,39 @@
 'use strict';
 
+/* global grecaptcha */
+
 require('hof/frontend/themes/gov-uk/client-js');
 const repeater = require('./repeater');
 const govuk = require('govuk-frontend');
+const reCaptchaSiteKeyV3 = window.reCaptchaSiteKeyV3;
 
 govuk.initAll();
 
 repeater.init();
+
+function onSubmit(e) {
+  e.preventDefault();
+  grecaptcha.enterprise.ready(async () => {
+    const token = await grecaptcha.enterprise.execute(reCaptchaSiteKeyV3, {action: 'submit'});
+
+    // Populate hidden field with token
+    const form = e.target.closest('form');
+    const hiddenField = form.querySelector('input[name="g-recaptcha-token"]');
+    if (hiddenField) {
+      hiddenField.value = token;
+    } else {
+      const newHiddenField = document.createElement('input');
+      newHiddenField.type = 'hidden';
+      newHiddenField.name = 'g-recaptcha-token';
+      newHiddenField.value = token;
+      form.appendChild(newHiddenField);
+    }
+
+    form.submit();
+  });
+}
+
+// Listen for submit on all forms
+document.querySelectorAll('form').forEach(form => {
+  form.addEventListener('submit', onSubmit);
+});
