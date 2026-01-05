e672b98e9fe2c8036d2ab8e1f5f70b9e5cb0c1c6
Mike Maratheftis <mike.maratheftis@digital.homeoffice.gov.uk>
2026-01-05 15:55:15 +0000

    ROTM-176 add unit tests for behaviour

--- test/_unit/apps/rotm/behaviours/url-repeater.spec.js
+++ test/_unit/apps/rotm/behaviours/url-repeater.spec.js
@@ -21,30 +21,53 @@ describe("apps/rotm 'url-repeater' behaviour should ", () => {
   beforeEach(() => {
     req = reqres.req();
     res = reqres.res();
+    next = sinon.spy();
+
+    req.sessionModel.get = sinon.stub();
+    req.sessionModel.set = sinon.spy();
+    req.log = sinon.spy();
   });
 
   describe('The \'saveValues\' method', () => {
     beforeEach(() => {
-      sinon.stub(Base.prototype, 'saveValues').returns(res, res, next);
+      sinon.stub(Base.prototype, 'saveValues').returns(res);
+
       instance = new (Behaviour(Base))();
-      req.form.values.urls = [
-        req.form.values.url,
-        (req.form.values['another-url-1'] = 'www.test1.com'),
-        (req.form.values['another-url-2'] = 'www.test2.com'),
-        (req.form.values['another-url-3'] = ''),
-        (req.form.values['another-url-4'] = '')
-      ];
+
+      req.form.values.url = 'www.test1.com';
+      req.form.values['another-url-1'] = 'www.test2.com';
+      req.form.values['another-url-2'] = '';
+      req.form.values['another-url-3'] = '';
+      req.form.values['another-url-4'] = '';
+
+      req.sessionModel.get.callsFake(key => {
+        if (key === 'submissionID') return '123';
+        if (key === 'ipaddress') return '127.0.0.1';
+        if (key === 'remote_port') return 8080;
+        return null;
+      });
+
       instance.saveValues(req, res, next);
     });
+
     it('should be called', () => {
       expect(Base.prototype.saveValues).to.have.been.called;
     });
+
     it('eliminates empty urls from the form', () => {
-      expect(req.form.values.urls)
-        .to.be.an('array')
-        .that.does.not.include('');
-      expect(req.form.values.urls).to.have.lengthOf(2);
+      expect(req.form.values.urls).to.deep.equal([
+        'www.test1.com',
+        'www.test2.com'
+      ]);
     });
+
+    it('logs the remote port', () => {
+      expect(req.log).to.have.been.calledWith(
+        'info',
+        sinon.match(/Port: 8080/)
+      );
+    });
+
     afterEach(() => {
       Base.prototype.saveValues.restore();
     });
@@ -52,13 +75,25 @@ describe("apps/rotm 'url-repeater' behaviour should ", () => {
 
   describe('The \'getValues\' method', () => {
     beforeEach(() => {
-      sinon.stub(Base.prototype, 'getValues').returns(req, res, next);
+      sinon.stub(Base.prototype, 'getValues').callsFake((_req, _res, cb) => {
+        cb(null, {});
+      });
+
+      req.connection = { remotePort: 3000 };
+
       instance = new (Behaviour(Base))();
       instance.getValues(req, res, next);
     });
 
     it('should be called', () => {
       expect(Base.prototype.getValues).to.have.been.called;
+    });
+
+    it('saves the remote port in session', () => {
+      expect(req.sessionModel.set).to.have.been.calledWith(
+        'remote_port',
+        3000
+      );
     });
 
     afterEach(() => {
