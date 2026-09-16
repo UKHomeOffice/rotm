517f715920df32cd383550e17b7654b7a5874ee7
GrahamOsifoHO <Graham.Osifo@digital.homeoffice.gov.uk>
2026-09-16 16:42:05 +0100

    Rotm-240 Add playwright test

--- e2e-tests/steps/rotm.step.ts
+++ e2e-tests/steps/rotm.step.ts
@@ -2,7 +2,6 @@ import { expect } from '@playwright/test';
 import { createBdd } from 'playwright-bdd';
 import { test, Pages } from '../fixture/fixtures';
 import { ConstantsLib as c, getRotmApplicantForScenario, RotmApplicant } from '../utility-helper/constants-lib';
-// import { getRotmApplicantForScenario, RotmApplicant } from '../test-data/rotm-applicant';
 
 export const { Given, When, Then } = createBdd(test);
 
